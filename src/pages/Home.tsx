eb6a41c09fb7d9c7685e4d081bb8b02cf70c0052
Denis_Gershovich <denis.gershovich@gmail.com>
2022-09-26 16:58:35 +0300

    add homepage style and content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,23 @@
-
+import { Container, Button, Row, Col } from "react-bootstrap"
+import { Link } from 'react-router-dom'
 const Home = () => {
-    return <h1>home page</h1>
+    return <Container
+        style={{ height: '60vh', width: '100%' }}
+    >
+        <Container className="d-flex flex-column justify-content-center align-items-center "
+            style={{ height: '100%', width: '100%' }}
+        >
+            <Row className="mb-3">
+                <Col><h1>SHOP APP</h1></Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button><Link to='/store' style={{ color: 'white', textDecoration: 'none' }}>shop now</Link></Button>
+                </Col>
+            </Row>
+        </Container>
+    </Container>
+
 }
 
 export default Home
